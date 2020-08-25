12387ffe2145becf99f483b3485adb99f0f46176
niharika829 <ns1324y@gmail.com>
2020-08-25 22:08:13 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './components/style.css';
+import Portfolio from './components/portfolio/app';
+
+ReactDOM.render(<Portfolio />, document.getElementById('root'));
