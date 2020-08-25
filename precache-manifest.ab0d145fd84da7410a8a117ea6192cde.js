85cde38812456510efc9cff586a51203a377cc1b
niharika829 <ns1324y@gmail.com>
2020-08-25 22:14:07 +0530

    Updates

---
+++ precache-manifest.ab0d145fd84da7410a8a117ea6192cde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a60e377579072b2e9b854ce95ff93b",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "dd44a6d884aff10021b7",
+    "url": "/portfolio_react/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "863f7a257d77b81df623",
+    "url": "/portfolio_react/static/css/main.ab997b90.chunk.css"
+  },
+  {
+    "revision": "dd44a6d884aff10021b7",
+    "url": "/portfolio_react/static/js/2.63da9ac8.chunk.js"
+  },
+  {
+    "revision": "ba26494cd66ee4b0fab146e4c2316d33",
+    "url": "/portfolio_react/static/js/2.63da9ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "863f7a257d77b81df623",
+    "url": "/portfolio_react/static/js/main.ec08c6b1.chunk.js"
+  },
+  {
+    "revision": "15751319bbd21f13abac",
+    "url": "/portfolio_react/static/js/runtime-main.452e50ea.js"
+  },
+  {
+    "revision": "828b39ee63bef5acbfa98efc22ec6643",
+    "url": "/portfolio_react/static/media/neha_resume.828b39ee.pdf"
+  }
+]);
